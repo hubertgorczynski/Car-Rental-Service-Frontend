dab7d957bc45b14b33688d12ef7f7fad6ed2152b
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-09-04 20:58:37 +0200

    First commit

---
+++ src/main/java/com/kodilla/project/KodillaProjectFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaProjectFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaProjectFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/project/KodillaProjectFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaProjectFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
